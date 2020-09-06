001f5836ff70f7b54b9b353c766e6d3ae20ea8b0
sulbha29 <52119985+sulbha29@users.noreply.github.com>
2020-09-06 10:43:07 +0530

    Add files via upload

---
+++ components/appDrawerNavigator.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { AppTabNavigator } from "./apptabnavigator";
+import CustomSidebar from "./customsidebar";
+import Settingscreen from "../screens/settingscreen";
+import Mydonationscreen from "../screens/mydonationscreen";
+export const appDrawernavigator = createDrawerNavigator(
+  {
+    home: { screen: AppTabNavigator },
+    mydonations:{
+      screen:Mydonationscreen
+    },
+    settings: { screen: Settingscreen }
+  },
+  {
+    contentComponent: CustomSidebar
+  },
+  {
+    initialRouteName: "home"
+  }
+);
